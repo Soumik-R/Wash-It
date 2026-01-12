3e80776184ba68b916e51d4068ae1cd7bbe238d9
SOUMIK ROY <soumikroy7272@gmail.com>
2026-01-13 03:24:22 +0530

    Updated server

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Wash-It Backend Running");
