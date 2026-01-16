80161eed044d7e3b5c7e64274d61b83d763ee928
SOUMIK ROY <soumikroy7272@gmail.com>
2026-01-17 01:58:14 +0530

    Added graph algorithm Djisktra

---
+++ backend/utils/graph.js
@@ -0,0 +1,18 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(v1, v2, weight) {
+    this.adjacencyList[v1].push({ node: v2, weight });
+    this.adjacencyList[v2].push({ node: v1, weight });
+  }
+}
+
+module.exports = Graph;
